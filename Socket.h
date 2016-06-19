259d5cd1164146de58b60a171108e68fa3b946a8
KMakowsky <apfelkay@googlemail.com>
2016-06-19 13:47:05 +0200

    Add files via upload

--- Socket.h
+++ Socket.h
@@ -9,16 +9,18 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 #include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 #include <netinet/in.h>
+#include <fcntl.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <vector>
+#include <sys/un.h>
 
 using namespace std;
 
@@ -37,10 +39,13 @@ public:
     Socket* accept();
     int socket_write(string msg);
     int socket_read(string &buf,int len);
+    int socket_safe_read(string &buf,int len,int seconds);
     int socket_writeTo(string msg, string ip, string port);
     int socket_readFrom(string &buf, int len, string ip, string port);
     int socket_set_opt(int level, int optname, void* optval);
     int socket_get_opt(int level, int optname, void* optval);
+    int set_blocking();
+    int set_non_blocking();
     int socket_shutdown(int how);
     void close();
     static int select(vector<Socket> *reads, vector<Socket> *writes, vector<Socket> *exceptions,int seconds);
